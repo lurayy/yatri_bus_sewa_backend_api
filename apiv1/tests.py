0d4c3b1ab91e1feb3dbb93a5ba5226978efe94f2
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2019-10-14 14:39:20 +0545

    Adds test cases for layout

--- apiv1/tests.py
+++ apiv1/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+''' Testing module for api '''
 
-# Create your tests here.
+from django.test import TestCase
+from .models import Layout, Seat
+
+
+class LayoutTestCase(TestCase):
+    ''' Class for testing layout model '''
+
+    def setUp(self):
+        layout = Layout.objects.create(name="test_layout", seat_count=22)
+        Seat.objects.create(seat_number=1, state='available', layout=layout)
+        Seat.objects.create(seat_number=2, state='available', layout=layout)
+
+    def test_layout(self):
+        ''' Creates a layout and add two seats then verify if the seat count is 2 or not '''
+        layout = Layout.objects.get(name="test_layout")
+        for seat in layout.seat_set.all():
+            print(seat)
+        self.assertEqual(layout.seat_set.count(), 2)
