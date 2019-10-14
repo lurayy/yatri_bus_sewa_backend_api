274af0f9ca500c7c0e1a772948bcf4161d2fb1f2
Kabir S. Tamari <kabirtamari@gmail.com>
2019-10-14 17:56:33 +0545

    added layout_to_json function in apiv1.utils

--- apiv1/urls.py
+++ apiv1/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('layouts', views.layouts, name='layouts')
+    path('layouts/', views.layouts, name='layouts'),
 ]
--- apiv1/utils.py
+++ apiv1/utils.py
@@ -1,6 +1,8 @@
 ''' Utility module '''
-from django.forms.models import model_to_dict as django_model_to_dict
 from .models import Layout, Seat
+
+from django.forms.models import model_to_dict as django_model_to_dict
+
 
 def model_to_dict(class_name):
     ''' Returns the dictionary of dialog objects '''
@@ -10,41 +12,31 @@ def model_to_dict(class_name):
         object_list.append(django_model_to_dict(obj))
     return object_list
 
-# data = {
-#   "name": "hello world",
-#   "data": [
-#     [
-#       {
-#         "state": "available"
-#       },
-#       {
-#         "state": "available"
-#       }
-#     ],
-#     [
-#       {
-#         "state": "available"
-#       },
-#       {
-#         "state": "none"
-#       }
-#     ]
-#   ]
-# }
 
-# def layout_to_json(layout):
-#     seats = Seat.objects.filter(layout = layout)
-#     response_json = {
-#         'name': layout.name,
-#         'data':[
-#             [
-
-#             ]
-#         ]
-#     }
-#     for seat in seats:
-        
-
-
-# def json_to_layout(data):
-#     pass
+def layout_to_json(layout):
+    ''' Takes in layout objects and gives all the data related to layout and it's seats'''
+    seats = Seat.objects.filter(layout=layout)
+    response_json = {
+        'name':layout.name,
+        'data': [],
+        }
+    position_x = []
+    position_y = []
+    states = []
+    for seat in seats:
+        position_x.append(int(seat.row))
+        position_y.append(int(seat.col))
+        states.append(str(seat.state))
+    for temp_x in range(max(position_x)+1):
+        print(temp_x)
+        response_json['data'].append([])
+        for _ in range(max(position_y)+1):
+            response_json['data'][temp_x].append(
+                {
+                    'state':"none"
+                }
+            )
+    for n in range(len(states)):
+        response_json['data'][position_x[n]][position_y[n]]['state'] = states[n]
+    return response_json
+    
