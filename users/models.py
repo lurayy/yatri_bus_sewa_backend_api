e8e92523fc2d547faf47757b64ed75f0cd9e63ce
Kabir S. Tamari <kabirtamari@gmail.com>
2019-10-16 01:05:08 +0545

    commenting user.models since we won't be working on that for a while

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
+# # Create your models here.
 
-# Create your models here.
+# class CustomUserBase(AbstractUser):
+#     is_operator = models.BooleanField(default= False)
+#     is_agent = models.BooleanField(default= False)
+#     is_customer = models.BooleanField(default= True)
+
+# class Operator(models.Model):
+#     REQUIRED_FIELDS = ('user',)
+#     user = models.OneToOneField(CustomUser, on_delete = models.CASCADE, primary_key = True, related_name = 'operator', unique = True)
