1d2762693e68adc8e1123950ac8cf413c9c3f3aa
Kabir S. Tamari <kabirtamari@gmail.com>
2019-10-15 20:42:47 +0545

    users module linked to main app

---
+++ users/urls.py
@@ -0,0 +1,7 @@
+''' Module containing the url patterns of users '''
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+]
--- yatri_bus/settings.py
+++ yatri_bus/settings.py
@@ -32,6 +32,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
     'apiv1',
     'corsheaders',
     'django.contrib.admin',
@@ -47,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
