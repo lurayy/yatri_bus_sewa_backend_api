b52f647755e7e17c21576e2f9514d675895ad357
Kabir Tamari <kabirtamari@gmail.com>
2020-02-14 15:36:20 +0545

    debuging

--- yatri_bus/urls.py
+++ yatri_bus/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/doc/', include('django.contrib.admindocs.urls')),
+    path('doc/', include('django.contrib.admindocs.urls')),
     path('', admin.site.urls),
     path('api/v1/', include('apiv1.urls')),
 ]
