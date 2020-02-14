6c112b54fb742628c03dddd468ad547f45bb9888
Kabir Tamari <kabirtamari@gmail.com>
2020-02-14 15:25:49 +0545

    done

--- yatri_bus/settings.py
+++ yatri_bus/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'udae!#@lk3tr)jr@bvva0@+t_8h#60h0il*-g-#^o!5_=acek1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['https://yatriapi.herokuapp.com/', 'localhost']
 CORS_ORIGIN_ALLOW_ALL = True
 
 
